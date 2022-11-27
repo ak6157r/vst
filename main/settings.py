3008afec4c31ded9f96685f62fc71aa3b94322c7
Anoop <ak6157r@gmail.com>
2022-11-28 00:25:07 +0530

    csrf_trusted_origins

--- main/settings.py
+++ main/settings.py
@@ -113,7 +113,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['web-production-7f45.up.railway.app']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
